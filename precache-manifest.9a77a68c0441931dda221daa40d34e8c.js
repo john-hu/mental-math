723a58c4f2002ffc0e3c140828c96bbe331612b8
John Hu <im@john.hu>
2019-08-05 11:20:37 +0800

    Updates

---
+++ precache-manifest.9a77a68c0441931dda221daa40d34e8c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cb8cc20e27e49b9dfc2db69637bed6",
+    "url": "/mental-math/index.html"
+  },
+  {
+    "revision": "e8c6c446f770d55ba47d",
+    "url": "/mental-math/static/js/2.43c523c4.chunk.js"
+  },
+  {
+    "revision": "5fd0c7906511f42fd784",
+    "url": "/mental-math/static/js/main.918cf6b9.chunk.js"
+  },
+  {
+    "revision": "899c77022587153e4ed3",
+    "url": "/mental-math/static/js/runtime~main.a41aa6f9.js"
+  }
+]);
